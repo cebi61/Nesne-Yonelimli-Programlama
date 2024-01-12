e99fa08aff930858e529c705562b4b2365fb1f54
Adil Çebi <adilhamza.cebi@ogrenci.bartin.edu.tr>
2024-01-12 05:26:03 +0300

    Create class-vs-instance.py

---
+++ OOP/class-vs-instance.py
@@ -0,0 +1,18 @@
+#sınıf ve nesne değişkenleri
+
+class calisan:
+    personel_sayisi = 0
+    def __init__(self,isim,maas):
+        self.isim = isim
+        self.maas = maas
+        calisan.personel_sayisi += 1
+
+print(calisan.personel_sayisi)
+calisan1 = calisan("Ali",5000)
+print(calisan.personel_sayisi)
+calisan2 = calisan("Ahmet",6000)
+print(calisan.personel_sayisi)
+
+print(calisan1.__dict__)
+print(calisan2.__dict__)
+
